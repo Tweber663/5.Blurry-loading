be36f28d975061a5c37515224297585c2f2a6ee4
Tweber663 <tweber.1995@hotmail.com>
2024-09-11 14:58:55 +0200

    html, css and JS added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,17 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const bg = document.querySelector('.photoBox');
+const percentage = document.querySelector('.loading-text');
+let count = 0;
+const increasement = () => {
+    count++;
+    if (count == 100)
+        clearInterval(counting);
+    percentage.innerText = String(`${count}%`);
+    percentage.style.opacity = String(scale(count, [0, 100], [1, 0]));
+    bg.style.filter = `blur(${String(scale(count, [0, 100], [30, 0]))}px)`;
+};
+const scale = (number, [inMin, inMax], [outMin, outMax]) => {
+    return (number - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+};
+const counting = setInterval(increasement, 10);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
-import Stopwatch from './StopwatchWithResults.js'
+const bg = document.querySelector('.photoBox') as HTMLDivElement; 
+const percentage = document.querySelector('.loading-text') as HTMLDivElement; 
 
-new Stopwatch(document.querySelector('.stopwatch'))
+let count = 0; 
+const increasement = () => {
+    count++)
+    if(count == 100) clearInterval(counting);
+    percentage.innerText = String(`${count}%`);
+    percentage.style.opacity = String(scale(count, [0, 100], [1, 0]));
+    bg.style.filter = `blur(${String(scale(count, [0, 100], [30, 0]))}px)`; 
+}; 
+
+const scale = (number: number, [inMin, inMax]: number[], [outMin, outMax]: number[]) => {
+    return (number - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+}
+
+
+const counting = setInterval(increasement, 10); 
+
